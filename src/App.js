3ba1db59e847e132752fb6d435a2cc5c389db88f
Dave Shevitz <shevitz@stripe.com>
2023-07-28 21:08:31 -0700

    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function EncounterBoard() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="encounter-board">
+      <h1>Dave's Encounter Tracker</h1>
+      <EncounterTable combatants={COMBATANTS} />
     </div>
-  );
+  )
 }
 
-export default App;
+function EncounterTable({ combatants }){
+  const rows = [];
+  combatants.forEach((combatant) => {
+    rows.push(
+      renderCombatant(combatant)
+    )
+   }
+  )
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Initiative</th>
+          <th>Type</th>
+          <th>Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
+}
+
+function renderCombatant( combatant) {
+  return(
+    <Combatant
+      key={combatant.name}
+      name={combatant.name}
+      initiative={combatant.initiative}
+      type={combatant.type}
+      number={combatant.number}
+      />
+  )
+}
+
+function Combatant({ name, initiative, type, number }){
+  return(
+      <tr id={name}>
+        <td>{name}</td>
+        <td>{initiative}</td>
+        <td>{type}</td>
+        <td>{number}</td>
+      </tr>
+  )
+}
+
+const COMBATANTS = [
+  {name: "Dolor", initiative: 10, type: "player", number: 1},
+  {name: "Gven", initiative: 15, type: "player", number: 1},
+  {name: "Brindle", initiative: 7, type: "player", number: 1},
+  {name: "Mond", initiative: 18, type: "player", number: 1},
+  {name: "Gnoll", initiative: 3, type: "enemy", number: 3},
+  {name: "Gnoll Pack Lord", initiative: 8, type: "enemy", number: 1},
+  {name: "Lyra Swiftarrow", initiative: 21, type: "npc", number: 1}
+];
