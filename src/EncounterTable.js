7720f182f76dc55521b54f27cfbfa3ec3b315d47
Dave Shevitz <shevitz@stripe.com>
2023-07-31 20:15:00 -0700

    move components and data to separate files

---
+++ src/EncounterTable.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import Combatant from './Combatant';
+import AddCombatant from './AddCombatant';
+
+export default function EncounterTable({ combatants }){
+    const [combatantState, setCombatantState] = useState(combatants || []);
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [selectedCombatantId, setSelectedCombatantId] = useState(null);
+  
+    const handleDeletedCombatant = (combatantName) => {
+      const updatedCombatants = combatantState.filter((combatant) => combatant.name !== combatantName);
+      setCombatantState(updatedCombatants);
+      console.log("Deleted combatant:", combatantName);
+    };
+  
+    const handleToggleEditMode = () => {
+      setIsEditMode(!isEditMode);
+    };
+  
+    const handleInputChange = (event, index, field) => {
+      const { value } = event.target;
+      setCombatantState((prevCombatants) => {
+        const updatedCombatants = [...prevCombatants];
+        updatedCombatants[index][field] = value;
+        return updatedCombatants;
+      });
+    };
+    
+    const processFormData = (formData) => {
+      const newCombatant = {
+        id: Date.now(), // Generate a unique ID (you can use a library like uuid for a more reliable unique ID)
+        name: formData.name,
+        initiative: parseInt(formData.initiative),
+        type: formData.type,
+        currenthp: parseInt(formData.hitpoints),
+        totalhp: parseInt(formData.hitpoints),
+      };
+  
+      setCombatantState((prevCombatants) => [...prevCombatants, newCombatant]);
+    };
+  
+    const sortedCombatants = combatantState.slice().sort((a, b) => b.initiative - a.initiative);
+    
+    const rows = sortedCombatants.map((combatant, index) => (
+      <Combatant
+        key={combatant.name}
+        combatant={combatant}
+        onDeleteCombatant={handleDeletedCombatant}
+        isEditMode={isEditMode}
+        onInputChange={(event, field) => handleInputChange(event, index, field)}
+        isSelected={selectedCombatantId === combatant.id}
+        onSelectCombatant={() => setSelectedCombatantId(combatant.id)}
+      />
+    
+    ));
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Initiative</th>
+              <th>Type</th>
+              <th>Hit Points</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+        <button onClick={handleToggleEditMode}>
+          {isEditMode ? "Save" : "Edit Mode"}
+        </button>
+        <div>
+          <h2>Add New Combatant</h2>
+          <AddCombatant onSubmitForm={processFormData} />
+        </div>
+      </div>
+    )
+  }
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const COMBATANTS = [
+    {id: 1, name: "Dolor", initiative: 10, type: "player", currenthp: 25, totalhp: 25},
+    {id: 2, name: "Gven", initiative: 15, type: "player", currenthp: 35, totalhp: 35},
+    {id: 3, name: "Brindle", initiative: 7, type: "player", currenthp: 20, totalhp: 20},
+    {id: 4, name: "Mond", initiative: 18, type: "player", currenthp: 22, totalhp: 22},
+    {id: 5, name: "Gnoll", initiative: 3, type: "enemy", currenthp: 8, totalhp: 8},
+    {id: 6, name: "Gnoll Pack Lord", initiative: 8, type: "enemy", currenthp: 8, totalhp: 32},
+    {id: 7, name: "Lyra Swiftarrow", initiative: 21, type: "npc", currenthp: 45, totalhp: 45}
+  ];
