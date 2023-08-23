c04b2abf5e6460fa6a3c568784e7d4270dcca741
Dave Shevitz <shevitz@stripe.com>
2023-08-22 17:44:39 -0700

    remove button dependency

--- src/Combatant.js
+++ src/Combatant.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import { ArrowRightCircleFill } from 'react-bootstrap-icons';
 import { EmojiDizzy } from 'react-bootstrap-icons';
 import { XCircleFill } from 'react-bootstrap-icons';
